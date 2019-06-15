a48134478ee0f43086464dddf4f19f67be3a83bf
Shekhar <shekharnunia@gmail.com>
2019-06-15 18:40:48 +0530

    apps added into settings in installed apps and users model has created with admin site

--- rtucolleges/users/admin.py
+++ rtucolleges/users/admin.py
@@ -1,3 +1,46 @@
+from django import forms
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as AuthUserAdmin
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
 
-# Register your models here.
+from users.models import User
+
+
+class MyUserChangeForm(UserChangeForm):
+    class Meta(UserChangeForm.Meta):
+        model = User
+
+
+class MyUserCreationForm(UserCreationForm):
+
+    error_message = UserCreationForm.error_messages.update({
+        'duplicate_username': 'This username has already been taken.'
+    })
+
+    class Meta(UserCreationForm.Meta):
+        model = User
+
+    def clean_username(self):
+        username = self.cleaned_data["username"]
+        try:
+            User.objects.get(username=username)
+
+        except User.DoesNotExist:
+            return username
+
+        raise forms.ValidationError(self.error_messages['duplicate_username'])
+
+
+@admin.register(User)
+class MyUserAdmin(AuthUserAdmin):
+    form = MyUserChangeForm
+    add_form = MyUserCreationForm
+    fieldsets = (
+        ('User Profile', {'fields': ('name', 'is_student',
+                                     'is_teacher', 'location', 'job_title', 'personal_url',
+                                     'facebook_account', 'twitter_account', 'github_account',
+                                     'linkedin_account', 'short_bio', 'bio', 'picture', 'stack_overflow',
+                                     'reputation')}),
+    ) + AuthUserAdmin.fieldsets
+    list_display = ('username', 'name', 'is_superuser')
+    search_fields = ['name']
--- rtucolleges/users/models.py
+++ rtucolleges/users/models.py
@@ -1,3 +1,50 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
-
+from django.urls import reverse
+from django.utils.translation import ugettext_lazy as _
 # Create your models here.
+
+
+class User(AbstractUser):
+    pass
+    name = models.CharField(_("User's name"), blank=True, max_length=255)
+    picture = models.ImageField(
+        _('Profile picture'), upload_to='profile_pics/', null=True, blank=True)
+    location = models.CharField(
+        _('Location'), max_length=50, null=True, blank=True)
+    job_title = models.CharField(
+        _('Job title'), max_length=50, null=True, blank=True)
+    personal_url = models.URLField(
+        _('Personal URL'), max_length=555, blank=True, null=True)
+    facebook_account = models.URLField(
+        _('Facebook profile'), max_length=255, blank=True, null=True)
+    twitter_account = models.URLField(
+        _('Twitter account'), max_length=255, blank=True, null=True)
+    github_account = models.URLField(
+        _('GitHub profile'), max_length=255, blank=True, null=True)
+    stack_overflow = models.URLField(
+        _('Stackoverflow profile'), max_length=255, blank=True, null=True)
+    linkedin_account = models.URLField(
+        _('LinkedIn profile'), max_length=255, blank=True, null=True)
+    short_bio = models.CharField(
+        _('Describe yourself'), max_length=60, blank=True, null=True)
+    bio = models.CharField(
+        _('Short bio'), max_length=280, blank=True, null=True)
+    is_student = models.BooleanField('student status', default=False)
+    is_teacher = models.BooleanField('teacher status', default=False)
+    email_public = models.BooleanField(default=True)
+    reputation = models.IntegerField(default='1')
+
+    # Is_student
+    #     Student Rank
+    #     Branch
+    #     Experience
+    # Is_teacher
+    #     Techer Rank
+    #     Job title
+    #     Work Experience
+
+    # College name
+    # Badge(ex: Autobiographer - for completing profile details, First like, etc)
+    # Skills
+    # Accomplishments
